baaf7eac272da0d7402e08dffd45b68bef1fffca
Dare <edidiongekrikpo94@gmail.com>
2024-10-10 19:42:56 +0100

    backtohomepage

--- App.js
+++ App.js
@@ -11,10 +11,7 @@ import { Play_400Regular } from "@expo-google-fonts/play";
 import { ShareTechMono_400Regular } from "@expo-google-fonts/share-tech-mono";
 import { SpecialElite_400Regular } from "@expo-google-fonts/special-elite";
 
-import SettingsScreen from "./src/screens/SettingsScreen";
-import ProfileScreen from "./src/screens/ProfileScreen";
-import HomeScreen from "./src/screens/HomeScreen";
-import NotificationsScreen from "./src/screens/NotificationsScreen";
+import { HomeScreen } from "./src/screens/HomeScreen";
 import store from "./src/store";
 
 const Stack = createStackNavigator();
@@ -24,9 +21,6 @@ function Main() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Notifications" component={NotificationsScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -51,11 +45,3 @@ export default function App() {
     </Provider>
   );
 }
-
-export const Increase = () => ({
-  type: "INCREASE"
-});
-
-export const Decrease = () => ({
-  type: "DECREASE"
-});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,7 @@
-import { View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-import {
-  BatteryIcon,
-  DateDisplay,
-  IconButton,
-  MenuItem,
-  ModalMenu,
-  TimeDisplay
-} from "../components";
+import { PALETTE } from "../constants";
 
 // styles
 const styles = StyleSheet.create({
@@ -16,94 +10,13 @@ const styles = StyleSheet.create({
     backgroundColor: PALETTE.black,
     flex: 1,
     justifyContent: "space-evenly"
-  },
-  display: {
-    alignItems: "center",
-    paddingHorizontal: 4 * GAP
-  },
-  time: {
-    marginBottom: GAP
-  },
-  date: {
-    marginBottom: 2.5 * GAP
   }
 });
 
 //main screen
-
-export const HomeScreen = ({ navigation }) => {
-  //hooks
-  useKeepAwake();
-  const { t, i18n } = useTranslation("Home");
-  const orientation = useOrientation();
-  const level = useBatteryLevel();
-  const [showModalMenu, setShowModalMenu] = useState(false);
-  const [time, setTime] = useState(dayjs());
-  const { timeColor } = useTimeColor();
-  const { timeFont } = useTimeFont();
-  const { timeFormat } = useTimeFormat();
-  const { showSeconds } = useShowSeconds();
-  const { showDate } = useShowDate();
-  const { showBattery } = useShowBattery();
-
-  useEffect(() => {
-    const lang = i18n.resolvedLanguage;
-    if (SUPPORTED_LANGUAGES.includes(lang)) {
-      dayjs.locale(lang);
-    } else {
-      dayjs.locale(FALLBACK_LANGUAGE);
-    }
-  }, [i18n.resolvedLanguage]);
-
-  //effect that gets current local time before initial screen render
-  useEffect(() => {
-    const updateTime = () => setTime(dayjs());
-    // setInterval is not precise enough in some devices
-    // so I decided to use 1/3 seconds (approx. 333 milliseconds)
-    // to always show an accurate time
-    const i = setInterval(() => updateTime(), 333);
-    updateTime();
-    return () => clearInterval(i);
-  }, []);
-
+export const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.root}>
-      <View style={styles.display}>
-        <TimeDisplay
-          value={time}
-          color={timeColor}
-          format={timeFormat}
-          showSeconds={showSeconds}
-          font={timeFont}
-          style={styles.time}
-        />
-        {showDate && (
-          <DateDisplay value={time} color={timeColor} style={styles.date} />
-        )}
-        {showBattery && <BatteryIcon value={100 * level} color={timeColor} />}
-      </View>
-
-      {orientation === "portrait" && (
-        <IconButton
-          icon={<SettingsIcon width={36} height={32} fill={timeColor} />}
-          onPress={() => setShowModalMenu(true)}
-        />
-      )}
-
-      <ModalMenu open={showModalMenu} onClose={() => setShowModalMenu(false)}>
-        <MenuItem
-          label={t`preferences`}
-          onPress={() => navigation.navigate("SettingsScreen")}
-        />
-        <MenuItem
-          label={t`fonts`}
-          onPress={() => navigation.navigate("FontsScreen")}
-        />
-        <MenuItem
-          label={t`colors`}
-          onPress={() => navigation.navigate("ColorsScreen")}
-        />
-      </ModalMenu>
       <StatusBar hidden />
     </SafeAreaView>
   );
